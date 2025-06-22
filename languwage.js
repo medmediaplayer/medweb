ee3e7ac5f774c7f896d06cb5ccc5f575703fb054
shc2012 <shc20122024@outlook.com>
2025-06-22 10:20:56 +0800

    Add files via upload

---
+++ languwage.js
@@ -0,0 +1,9 @@
+function changeLanguage(lang) {
+    if (lang === 'zh') {
+        window.location.href = 'index.html';
+    } else if (lang === 'en') {
+        window.location.href = 'en.html';
+    } else if (lang === 'tw') {
+        window.location.href = 'tw.html';
+    }
+}
