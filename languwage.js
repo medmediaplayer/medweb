4fe0513b3942a16029aae26f6b572220f4d9c589
shc2012 <shc20122024@outlook.com>
2025-06-22 10:21:26 +0800

    Update languwage.js

--- languwage.js
+++ languwage.js
@@ -3,7 +3,7 @@ function changeLanguage(lang) {
         window.location.href = 'index.html';
     } else if (lang === 'en') {
         window.location.href = 'en.html';
-    } else if (lang === 'tw') {
-        window.location.href = 'tw.html';
+    } else if (lang === 'hk') {
+        window.location.href = 'hk.html';
     }
 }
