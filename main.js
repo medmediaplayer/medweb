a575de228f591d6b10d6894bc4ba384104d692c7
shc2012 <shc20122024@outlook.com>
2025-06-23 21:32:48 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function changeLanguage(lang) {
-    if (lang === 'zh') {
-        window.location.href = 'index.html';
-    } else if (lang === 'en') {
-        window.location.href = 'en.html';
-    } else if (lang === 'hk') {
-        window.location.href = 'hk.html';
-    }
-}
